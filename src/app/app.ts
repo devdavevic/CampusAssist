331ca7902bd5bc8423a70239966efb362791f770
kenneth malate <idararaphaeljames@gmail.com>
2026-03-14 09:07:35 +0800

    User Tables

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,15 +7,26 @@ import moment from 'moment';
   selector: 'app-root',
   standalone: true,
   imports: [FormsModule, CommonModule],
-  templateUrl: './app.html',   
-  styleUrls: ['./app.css']    
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
 })
 export class App {
-
   username: string = '';
   password: string = '';
+  newUsername: string = '';
+  newPassword: string = '';
+
   greeting: string = '';
   loginTime: string = '';
+  isLoginMode: boolean = true;
+
+  accounts: { 
+    username: string; 
+    password: string; 
+    email: string; 
+    course: string; 
+    yearSection: string; 
+  }[] = [];
 
   login() {
     if (this.username && this.password) {
@@ -23,4 +34,30 @@ export class App {
       this.loginTime = moment().format('MMMM Do YYYY, h:mm:ss A');
     }
   }
+
+  signup() {
+    if (this.newUsername && this.newPassword) {
+      this.greeting = `Account created for ${this.newUsername}`;
+      this.loginTime = moment().format('MMMM Do YYYY, h:mm:ss A');
+
+      
+      this.accounts.push({
+        username: this.newUsername,
+        password: this.newPassword,
+        email: `${this.newUsername.toLowerCase()}@example.com`,
+        course: 'BSIT',
+        yearSection: '3-A'
+      });
+
+      this.newUsername = '';
+      this.newPassword = '';
+      this.isLoginMode = true;
+    }
+  }
+
+  switchMode() {
+    this.isLoginMode = !this.isLoginMode;
+    this.greeting = '';
+  }
 }
+
