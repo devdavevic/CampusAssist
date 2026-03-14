65ec256c7216dc81e5a64fad79f425ef1edafcfb
kenneth malate <idararaphaeljames@gmail.com>
2026-03-14 10:42:15 +0800

    Act 7: Form Controls

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import moment from 'moment';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule, ReactiveFormsModule],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
@@ -15,10 +16,14 @@ export class App {
   password: string = '';
   newUsername: string = '';
   newPassword: string = '';
+  signupForm: FormGroup;
 
   greeting: string = '';
   loginTime: string = '';
   isLoginMode: boolean = true;
+  showDashboard: boolean = false;
+  currentPage: string = 'dashboard';
+  
 
   accounts: { 
     username: string; 
@@ -28,32 +33,52 @@ export class App {
     yearSection: string; 
   }[] = [];
 
-  login() {
-    if (this.username && this.password) {
-      this.greeting = `Hello ${this.username}`;
-      this.loginTime = moment().format('MMMM Do YYYY, h:mm:ss A');
-    }
+  constructor(private fb: FormBuilder) {
+  this.signupForm = this.fb.group({
+    newUsername: ['', [Validators.required, Validators.minLength(4), Validators.email]],
+    newPassword: ['', [Validators.required, Validators.minLength(6)]]
+  });
+}
+
+login() {
+  if (this.username && this.password) {
+    this.greeting = `Hello ${this.username}`;
+    this.loginTime = moment().format('MMMM Do YYYY, h:mm:ss A');
+    this.showDashboard = true;
+  }
+}
+
+navigate(page: string) {
+  this.currentPage = page;
+}
+
+signup() {
+
+  if (this.signupForm.invalid) {
+    this.signupForm.markAllAsTouched();
+    return;
   }
 
-  signup() {
-    if (this.newUsername && this.newPassword) {
-      this.greeting = `Account created for ${this.newUsername}`;
-      this.loginTime = moment().format('MMMM Do YYYY, h:mm:ss A');
+  const username = this.signupForm.get('newUsername')?.value;
+  const password = this.signupForm.get('newPassword')?.value;
 
-      
-      this.accounts.push({
-        username: this.newUsername,
-        password: this.newPassword,
-        email: `${this.newUsername.toLowerCase()}@example.com`,
-        course: 'BSIT',
-        yearSection: '3-A'
-      });
+  console.log(username);
+  console.log(password);
 
-      this.newUsername = '';
-      this.newPassword = '';
-      this.isLoginMode = true;
-    }
-  }
+  this.greeting = `Account created for ${username}`;
+  this.loginTime = moment().format('MMMM Do YYYY, h:mm:ss A');
+
+  this.accounts.push({
+    username: username,
+    password: password,
+    email: `${username.toLowerCase()}@example.com`,
+    course: 'BSIT',
+    yearSection: '3-A'
+  });
+
+  this.signupForm.reset();
+  this.isLoginMode = true;
+}
 
   switchMode() {
     this.isLoginMode = !this.isLoginMode;
